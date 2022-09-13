22903a4eb8f8841bd2d8a7956abb380966766881
Andrey Marymov <marymovandrey078115@gmail.com>
2022-09-13 10:24:38 +0300

    Code Template

---
+++ CodeTemplate/Program.cs
@@ -0,0 +1,20 @@
+﻿int[,] GetArray(int m, int n, int minValue, int maxValue) {
+  int[,] result = new int[m, n];
+
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+
+  return result;
+}
+
+void PrintArray(int[,] array) {
+  for (int i = 0; i < array.GetLength(0); ++i) {
+    for (int j = 0; j < array.GetLength(1); ++j) {
+      Console.Write(array[i, j]);
+    }
+    Console.WriteLine();
+  }
+}
